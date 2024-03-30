33276411c99255cd3fadf2c7bdf95c42b2837cbf
Matthew Yu <myu2@umbc.edu>
2024-03-30 01:04:05 -0400

    search bar

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let tester = document.querySelector('.test');
 let img = document.querySelector('.test2');
 
+let pokemonSearch = document.querySelector('#pokemon-search');
+let pokemonForm = document.querySelector('.input-form');
+let curPokemon = 'pikachu';
+
 const fetchPokemon = async () => {
-    const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon/1/');
-    const data = await pokemon.json();
+    try {
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${curPokemon}/`);
+        const data = await pokemon.json();
 
-    console.log(data);
+        console.log(data);
 
-    tester.textContent = data.name
-    img.src=data.sprites.front_default
+        tester.textContent = data.name
+        img.src=data.sprites.front_default
+        
+    } catch (error) {
+        pokemonSearch.style.border = '2px solid red';
+    }
 }
 
-fetchPokemon();
+pokemonForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    curPokemon = pokemonSearch.value.trim().toLowerCase();
+    fetchPokemon();
+})
