3a70e477b833ea4d8468f1117fa4e45d438a6800
Matthew Yu <myu2@umbc.edu>
2024-03-31 19:54:28 -0400

    added types

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ let pokemonSearch = document.querySelector('#pokemon_search');
 let pokemonForm = document.querySelector('.input_form');
 let curPokemon = 'pikachu';
 
+const types = {
+    fire: '#e63946',
+    electric: '#ffbe0b',
+    water: '#0077b6',
+    grass: '#588157',
+    ground: '#d4a373',
+    fighting: '#eb5e28',
+    ice: '#caf0f8',
+    ghost: '#8f2d56',
+    psychic: '#f15bb5',
+    rock: '#6c584c',
+    poison: '#6d597a',
+    flying: '#5e4ae3',
+    dragon: '#05299e',
+    bug: '#ecf39e',
+    normal: '#e0e1dd',
+    steel: '#c0c0c0',
+    fairy: '#ffc6ff',
+    dark: '#000000'
+};
+
 const fetchPokemon = async () => {
     try {
         const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${curPokemon}/`);
@@ -14,12 +35,17 @@ const fetchPokemon = async () => {
 
         pokemon_name.textContent = data.name.toUpperCase();
         img.src = data.sprites.front_default
-        
+        checkType(data.types[0].type.name);
+
     } catch (error) {
         pokemonSearch.style.border = '2px solid red';
     }
 }
 
+const checkType = (type) => {
+    console.log(type)
+    pokemon_name.style.setProperty('--type', types[type])
+}
 
 pokemonForm.addEventListener('submit', async(e) => {
     e.preventDefault();
